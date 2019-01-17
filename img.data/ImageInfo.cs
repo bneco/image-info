b7d3da2764185d251cfbe28de53d97fb6c89b1be
bneco <46791781+bneco@users.noreply.github.com>
2019-01-17 17:32:09 +0000

    Add files via upload

---
+++ img.data/ImageInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace img.data
+{
+    public class ImageInfo
+    {
+        public string Property { get; set; }
+        public string Value { get; set; }
+    }
+}
